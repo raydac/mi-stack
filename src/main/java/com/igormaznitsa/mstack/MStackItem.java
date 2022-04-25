076368758292036ec5ffa98a33b7b6771749b348
Igor Maznitsa <rrg4400@gmail.com>
2022-04-25 10:11:44 +0300

    initial

---
+++ src/main/java/com/igormaznitsa/mstack/MStackItem.java
@@ -0,0 +1,4 @@
+package com.igormaznitsa.mstack;
+
+public class MStackItem {
+}
---
+++ src/test/java/com/igormaznitsa/mstack/MStackTest.java
@@ -0,0 +1,7 @@
+package com.igormaznitsa.mstack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MStackTest {
+
+}
