790988542d35f4537747dddc009ed3bfe32da9a2
Igor Maznitsa <rrg4400@gmail.com>
2022-05-18 12:43:32 +0300

    refactoring

--- src/test/java/com/igormaznitsa/mistack/MiStackArrayListTest.java
+++ src/test/java/com/igormaznitsa/mistack/MiStackArrayListTest.java
@@ -2,7 +2,7 @@ package com.igormaznitsa.mistack;
 
 import com.igormaznitsa.mistack.impl.MiStackArrayList;
 
-public class MiStackArrayListTest extends AbstractMiStackTest {
+class MiStackArrayListTest extends AbstractMiStackTest {
   @Override
   MiStack makeStack() {
     return new MiStackArrayList();
