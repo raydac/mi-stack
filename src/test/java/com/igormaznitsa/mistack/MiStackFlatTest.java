704d1b4d4781ab1b0b8dc15493cc782aefbaea2c
Igor Maznitsa <rrg4400@gmail.com>
2024-07-15 10:12:34 +0300

    refactoring

--- src/test/java/com/igormaznitsa/mistack/MiStackFlatTest.java
+++ src/test/java/com/igormaznitsa/mistack/MiStackFlatTest.java
@@ -27,103 +27,106 @@ class MiStackFlatTest extends AbstractMiStackTest {
 
   @Test
   void testIterateOverEmpty() {
-    final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null);
-    assertFalse(flatStack.iterator().hasNext());
+    try (final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null)) {
+      assertFalse(flatStack.iterator().hasNext());
+    }
   }
 
   @Test
   void testIterateWithCut() {
-    final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null);
+    try (final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null)) {
 
-    final MiStack<String> stack1 = new MiStackArrayList<>("stack1");
-    stack1.push(itemOf("item3", tagOf("A")));
-    stack1.push(itemOf("item2", tagOf("A")));
-    stack1.push(itemOf("item1", tagOf("A")));
+      final MiStack<String> stack1 = new MiStackArrayList<>("stack1");
+      stack1.push(itemOf("item3", tagOf("A")));
+      stack1.push(itemOf("item2", tagOf("A")));
+      stack1.push(itemOf("item1", tagOf("A")));
 
-    final MiStack<String> stack2 = new MiStackArrayList<>("stack2");
-    stack2.push(itemOf("item6", tagOf("B")));
-    stack2.push(itemOf("item5", tagOf("B")));
-    stack2.push(itemOf("item4", tagOf("B")));
+      final MiStack<String> stack2 = new MiStackArrayList<>("stack2");
+      stack2.push(itemOf("item6", tagOf("B")));
+      stack2.push(itemOf("item5", tagOf("B")));
+      stack2.push(itemOf("item4", tagOf("B")));
 
-    final MiStack<String> stack3 = new MiStackArrayList<>("stack3");
-    stack3.push(itemOf("item9", tagOf("C")));
-    stack3.push(itemOf("item8", tagOf("C")));
-    stack3.push(itemOf("item7", tagOf("C")));
+      final MiStack<String> stack3 = new MiStackArrayList<>("stack3");
+      stack3.push(itemOf("item9", tagOf("C")));
+      stack3.push(itemOf("item8", tagOf("C")));
+      stack3.push(itemOf("item7", tagOf("C")));
 
-    flatStack.pushStack(stack3).pushStack(stack2).pushStack(stack1);
+      flatStack.pushStack(stack3).pushStack(stack2).pushStack(stack1);
 
-    final List<String> found = new ArrayList<>();
-    var iterator = flatStack.iterator(x -> true, x -> !x.getValue().equals("item5"));
-    while (iterator.hasNext()) {
-      found.add(iterator.next().getValue());
+      final List<String> found = new ArrayList<>();
+      var iterator = flatStack.iterator(x -> true, x -> !x.getValue().equals("item5"));
+      while (iterator.hasNext()) {
+        found.add(iterator.next().getValue());
+      }
+      assertArrayEquals(new String[] {"item1", "item2", "item3", "item4"}, found.toArray(
+          String[]::new));
     }
-    assertArrayEquals(new String[] {"item1", "item2", "item3", "item4"}, found.toArray(
-        String[]::new));
   }
 
   @Test
   void testIterateWithFiltering() {
-    final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null);
+    try (final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null)) {
 
-    final MiStack<String> stack1 = new MiStackArrayList<>("stack1");
-    stack1.push(itemOf("item3", tagOf("A")));
-    stack1.push(itemOf("item2", tagOf("A")));
-    stack1.push(itemOf("item1", tagOf("A")));
+      final MiStack<String> stack1 = new MiStackArrayList<>("stack1");
+      stack1.push(itemOf("item3", tagOf("A")));
+      stack1.push(itemOf("item2", tagOf("A")));
+      stack1.push(itemOf("item1", tagOf("A")));
 
-    final MiStack<String> stack2 = new MiStackArrayList<>("stack2");
-    stack2.push(itemOf("item6", tagOf("B")));
-    stack2.push(itemOf("item5", tagOf("B")));
-    stack2.push(itemOf("item4", tagOf("B")));
+      final MiStack<String> stack2 = new MiStackArrayList<>("stack2");
+      stack2.push(itemOf("item6", tagOf("B")));
+      stack2.push(itemOf("item5", tagOf("B")));
+      stack2.push(itemOf("item4", tagOf("B")));
 
-    final MiStack<String> stack3 = new MiStackArrayList<>("stack3");
-    stack3.push(itemOf("item9", tagOf("C")));
-    stack3.push(itemOf("item8", tagOf("C")));
-    stack3.push(itemOf("item7", tagOf("C")));
+      final MiStack<String> stack3 = new MiStackArrayList<>("stack3");
+      stack3.push(itemOf("item9", tagOf("C")));
+      stack3.push(itemOf("item8", tagOf("C")));
+      stack3.push(itemOf("item7", tagOf("C")));
 
-    flatStack.pushStack(stack3).pushStack(stack2).pushStack(stack1);
+      flatStack.pushStack(stack3).pushStack(stack2).pushStack(stack1);
 
-    final MiStackTag filteredTag = tagOf("B");
+      final MiStackTag filteredTag = tagOf("B");
 
-    final List<String> found = new ArrayList<>();
-    var iterator = flatStack.iterator(x -> !x.getTags().contains(filteredTag));
-    while (iterator.hasNext()) {
-      found.add(iterator.next().getValue());
+      final List<String> found = new ArrayList<>();
+      var iterator = flatStack.iterator(x -> !x.getTags().contains(filteredTag));
+      while (iterator.hasNext()) {
+        found.add(iterator.next().getValue());
+      }
+      assertArrayEquals(new String[] {"item1", "item2", "item3", "item7", "item8", "item9"},
+          found.toArray(
+              String[]::new));
     }
-    assertArrayEquals(new String[] {"item1", "item2", "item3", "item7", "item8", "item9"},
-        found.toArray(
-            String[]::new));
   }
 
 
   @Test
   void testIterateOverAllFlattenStacks() {
-    final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null);
+    try (final MiStackFlat<String> flatStack = new MiStackFlat<>("testFlat", null)) {
 
-    final MiStack<String> stack1 = new MiStackArrayList<>("stack1");
-    stack1.push(itemOf("item3", tagOf("A")));
-    stack1.push(itemOf("item2", tagOf("A")));
-    stack1.push(itemOf("item1", tagOf("A")));
+      final MiStack<String> stack1 = new MiStackArrayList<>("stack1");
+      stack1.push(itemOf("item3", tagOf("A")));
+      stack1.push(itemOf("item2", tagOf("A")));
+      stack1.push(itemOf("item1", tagOf("A")));
 
-    final MiStack<String> stack2 = new MiStackArrayList<>("stack2");
-    stack2.push(itemOf("item6", tagOf("B")));
-    stack2.push(itemOf("item5", tagOf("B")));
-    stack2.push(itemOf("item4", tagOf("B")));
+      final MiStack<String> stack2 = new MiStackArrayList<>("stack2");
+      stack2.push(itemOf("item6", tagOf("B")));
+      stack2.push(itemOf("item5", tagOf("B")));
+      stack2.push(itemOf("item4", tagOf("B")));
 
-    final MiStack<String> stack3 = new MiStackArrayList<>("stack3");
-    stack3.push(itemOf("item9", tagOf("C")));
-    stack3.push(itemOf("item8", tagOf("C")));
-    stack3.push(itemOf("item7", tagOf("C")));
+      final MiStack<String> stack3 = new MiStackArrayList<>("stack3");
+      stack3.push(itemOf("item9", tagOf("C")));
+      stack3.push(itemOf("item8", tagOf("C")));
+      stack3.push(itemOf("item7", tagOf("C")));
 
-    flatStack.pushStack(stack3).pushStack(stack2).pushStack(stack1);
+      flatStack.pushStack(stack3).pushStack(stack2).pushStack(stack1);
 
-    var iterator = flatStack.iterator();
-    int index = 1;
-    while (iterator.hasNext()) {
-      final MiStackItem<String> nextValue = iterator.next();
-      assertEquals("item" + index, nextValue.getValue());
-      index++;
+      var iterator = flatStack.iterator();
+      int index = 1;
+      while (iterator.hasNext()) {
+        final MiStackItem<String> nextValue = iterator.next();
+        assertEquals("item" + index, nextValue.getValue());
+        index++;
+      }
+      assertEquals(10, index);
     }
-    assertEquals(10, index);
-
   }
 }
